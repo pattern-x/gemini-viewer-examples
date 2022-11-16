02a70f8de7e22a7fbc033a9ef42e38fe6eaff81c
“李仕蓬” <lishipeng3@gemdale.com>
2022-11-16 14:35:24 +0800

    fix:homepage and update ci

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <HashRouter basename="">
             <App />
       </HashRouter>
   </React.StrictMode>
